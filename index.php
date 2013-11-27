cf1b2f6e861533250ca4bb2d2dfdb047589b2fbe
unknown <dstoeltzing@LEG212-FA-9JW7.edison.edu>
2013-11-27 15:03:21 -0500

    Added first bits of vertical responsiveness

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <head>
     <title>Dan Stoeltzing: Web Developer, Web Designer, Producer</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0;">
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <script src="assets/js/jquery.min.js"></script>
